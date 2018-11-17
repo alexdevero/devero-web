b6f6a27df90d45c582852d69ee320ae87af4beac
Alex Devero <deveroalex@gmail.com>
2018-11-17 11:05:24 +0100

    Add Gatsby config files

---
+++ gatsby-config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  siteMetadata: {
+    title: 'DEVERO Corporation'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require(`postcss-import`)(),
+          require(`postcss-extend`)(),
+          require(`postcss-nesting`)(),
+          require('postcss-pxtorem')({
+            mediaQuery: false,
+            minPixelValue: 0,
+            propWhiteList: [],
+            replace: true,
+            rootValue: 16,
+            selectorBlackList: ['html'],
+            unitPrecision: 5
+          }),
+          require(`postcss-preset-env`)({ stage: 3 }),
+          require(`cssnano`)()
+        ]
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ]
+}
