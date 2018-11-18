b440e36a146d72ba0a6842b6a0b32a8822f66bf0
Alex Devero <deveroalex@gmail.com>
2018-11-18 11:33:37 +0100

    Add dropdowns to main nav

--- src/components/header.js
+++ src/components/header.js
@@ -8,16 +8,76 @@ const Header = ({ siteTitle }) => (
     <div className="container">
       <nav className="nav">
         <ul className="header__list-links">
-          <li>
-            <Link to="/about-us/">About us</Link>
+          <li className="dropdown__wrapper">
+            <Link to="/about-us/" className="dropdown__trigger">
+              About us
+            </Link>
+
+            <ul className="dropdown__list">
+              <li className="dropdown__item">
+                <Link to="">Our story</Link>
+              </li>
+
+              <li className="dropdown__item">
+                <Link to="">Our brand</Link>
+              </li>
+
+              <li className="dropdown__item">
+                <Link to="">Our mission</Link>
+              </li>
+            </ul>
+          </li>
+
+          <li className="dropdown__wrapper">
+            <Link to="/brand-portfolio" className="dropdown__trigger">
+              Brand portfolio
+            </Link>
+
+            <ul className="dropdown__list">
+              <li className="dropdown__item">
+                <Link to="">AI</Link>
+              </li>
+
+              <li className="dropdown__item">
+                <Link to="">Financial</Link>
+              </li>
+
+              <li className="dropdown__item">
+                <Link to="">Foundation</Link>
+              </li>
+
+              <li className="dropdown__item">
+                <Link to="">Galactic</Link>
+              </li>
+
+              <li className="dropdown__item">
+                <Link to="">Genomics</Link>
+              </li>
+
+              <li className="dropdown__item">
+                <Link to="">Industries</Link>
+              </li>
+
+              <li className="dropdown__item">
+                <Link to="">Media</Link>
+              </li>
+
+              <li className="dropdown__item">
+                <Link to="">Studio</Link>
+              </li>
+
+              <li className="dropdown__item">
+                <Link to="">Technologies</Link>
+              </li>
+            </ul>
           </li>
 
           <li>
-            <Link to="/brand-portfolio">Brand portfolio</Link>
-          </li>
-
-          <li>
-            <a href="https://www.blog.alexdevero.com" rel="noopener noreferrer" target="_blank">
+            <a
+              href="https://www.blog.alexdevero.com"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
               Blog
             </a>
           </li>
@@ -29,25 +89,25 @@ const Header = ({ siteTitle }) => (
 
         <ul className="header__list-social">
           <li>
-            <a href="/" rel="noopener noreferrer" target="_blank">
+            <a href="/" target="_blank">
               <span className="fab fa-facebook-f" />
             </a>
           </li>
 
           <li>
-            <a href="/" rel="noopener noreferrer" target="_blank">
+            <a href="/" target="_blank">
               <span className="fab fa-twitter" />
             </a>
           </li>
 
           <li>
-            <a href="/" rel="noopener noreferrer" target="_blank">
+            <a href="/" target="_blank">
               <span className="fab fa-instagram" />
             </a>
           </li>
 
           <li>
-            <a href="/" rel="noopener noreferrer" target="_blank">
+            <a href="/" target="_blank">
               <span className="fab fa-linkedin" />
             </a>
           </li>
