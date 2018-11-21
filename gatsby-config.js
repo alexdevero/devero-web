2db52d9557077da61f96f850a506a5ce41735faa
Alex Devero <deveroalex@gmail.com>
2018-11-21 06:10:36 +0100

    Update website title and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,13 +37,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'DEVERO Corporation',
+        short_name: 'DEVERO',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#D30320',
+        theme_color: '#D30320',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png' // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
