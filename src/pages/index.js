6f65dd478ed7510de4ad7adb56dcd55841b73b19
Alex Devero <deveroalex@gmail.com>
2018-11-21 05:41:35 +0100

    Add card for Ventures, update syntax

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Welcome to The DEVERO Corporation</h1>
 
-    <p>
-      The DEVERO Corporation is the home of DEVERO. We support the growth of the
-      DEVERO brand by developing and nurturing valuable DEVERO businesses. We
-      aspire to change the world for good.
-    </p>
+    <p>The DEVERO Corporation is the home of DEVERO. We support the growth of the DEVERO brand by developing and nurturing valuable DEVERO businesses. We aspire to change the world for good.</p>
 
     <div className="dashboard row">
       <div className="col-md-6 col-lg-4">
@@ -21,10 +17,7 @@ const IndexPage = () => (
           <div className="company-box__info">
             <h2 className="company-box__heading">AI</h2>
 
-            <p className="company-box__description">
-              Division of DEVERO Corporation focused on research and development
-              of AI.
-            </p>
+            <p className="company-box__description">Division of DEVERO Corporation focused on research and development of AI.</p>
           </div>
         </Link>
       </div>
@@ -36,9 +29,7 @@ const IndexPage = () => (
           <div className="company-box__info">
             <h2 className="company-box__heading">Financial</h2>
 
-            <p className="company-box__description">
-              Division of DEVERO Corporation focused on banking and fintech.
-            </p>
+            <p className="company-box__description">Division of DEVERO Corporation focused on banking and fintech.</p>
           </div>
         </Link>
       </div>
@@ -50,10 +41,7 @@ const IndexPage = () => (
           <div className="company-box__info">
             <h2 className="company-box__heading">Foundation</h2>
 
-            <p className="company-box__description">
-              Division of DEVERO Corporation focused on social good and
-              philantrophy.
-            </p>
+            <p className="company-box__description">Division of DEVERO Corporation focused on social good and philantrophy.</p>
           </div>
         </Link>
       </div>
@@ -65,10 +53,7 @@ const IndexPage = () => (
           <div className="company-box__info">
             <h2 className="company-box__heading">Galactic</h2>
 
-            <p className="company-box__description">
-              Division of DEVERO Corporation focused on research and development
-              of space technologies.
-            </p>
+            <p className="company-box__description">Division of DEVERO Corporation focused on research and development of space technologies.</p>
           </div>
         </Link>
       </div>
@@ -80,10 +65,7 @@ const IndexPage = () => (
           <div className="company-box__info">
             <h2 className="company-box__heading">Genomics</h2>
 
-            <p className="company-box__description">
-              Division of DEVERO Corporation focused on research and development
-              of genomics and related technologies.
-            </p>
+            <p className="company-box__description">Division of DEVERO Corporation focused on research and development of genomics and related technologies.</p>
           </div>
         </Link>
       </div>
@@ -95,10 +77,7 @@ const IndexPage = () => (
           <div className="company-box__info">
             <h2 className="company-box__heading">Industries</h2>
 
-            <p className="company-box__description">
-              Division of DEVERO Corporation focused on bringing research to the
-              physical world.
-            </p>
+            <p className="company-box__description">Division of DEVERO Corporation focused on bringing research to the physical world.</p>
           </div>
         </Link>
       </div>
@@ -110,9 +89,7 @@ const IndexPage = () => (
           <div className="company-box__info">
             <h2 className="company-box__heading">Media</h2>
 
-            <p className="company-box__description">
-              Division of DEVERO Corporation focused on marketing and media.
-            </p>
+            <p className="company-box__description">Division of DEVERO Corporation focused on marketing and media.</p>
           </div>
         </Link>
       </div>
@@ -124,10 +101,7 @@ const IndexPage = () => (
           <div className="company-box__info">
             <h2 className="company-box__heading">Studio</h2>
 
-            <p className="company-box__description">
-              Division of DEVERO Corporation focused on design, development and
-              hosting.
-            </p>
+            <p className="company-box__description">Division of DEVERO Corporation focused on design, development and hosting.</p>
           </div>
         </Link>
       </div>
@@ -139,10 +113,19 @@ const IndexPage = () => (
           <div className="company-box__info">
             <h2 className="company-box__heading">Technologies</h2>
 
-            <p className="company-box__description">
-              Division of DEVERO Corporation focused on research and development
-              of innovative technologies.
-            </p>
+            <p className="company-box__description">Division of DEVERO Corporation focused on research and development of innovative technologies.</p>
+          </div>
+        </Link>
+      </div>
+
+      <div className="col-md-6 col-lg-4">
+        <Link to="/" className="company-box">
+          <div className="card-box__thumbnail" />
+
+          <div className="company-box__info">
+            <h2 className="company-box__heading">Ventures</h2>
+
+            <p className="company-box__description">Division of DEVERO Corporation focused on growing our portfolio of investments.</p>
           </div>
         </Link>
       </div>
