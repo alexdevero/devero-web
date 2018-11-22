0354e5ac4594b7695c7caae8e7955e405ea8cc10
Alex Devero <deveroalex@gmail.com>
2018-11-22 06:12:48 +0100

    Add starter pages for DEVERO divisions

---
+++ src/pages/devero-galactic.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const DeveroGalactic = () => (
+  <Layout>
+    <h1>Hi from the Devero Galactic page</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default DeveroGalactic
---
+++ src/pages/devero-industries.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const DeveroIndustries = () => (
+  <Layout>
+    <h1>Hi from the Devero Industries page</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default DeveroIndustries
