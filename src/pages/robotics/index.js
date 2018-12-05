eb8cb98edf7ac4a44830ea7be327c479bae7e78b
Alex Devero <deveroalex@gmail.com>
2018-12-05 06:56:46 +0100

    Add page for Devero Robotics

---
+++ src/pages/robotics/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../../components/layout'
+
+const DeveroRobotics = () => (
+  <Layout>
+    <h1>Hi from the Devero Robotics page</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default DeveroRobotics
