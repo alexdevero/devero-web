db5bcfae004739b917a75a20d9cec26628c8d531
Alex Devero <deveroalex@gmail.com>
2018-12-05 07:02:45 +0100

    Add forgotten import for icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import { IconAi, IconFoundation, IconGenomics, IconIndustries, IconTechnologies } from '../components/icons'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
