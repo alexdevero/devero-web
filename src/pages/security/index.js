212bdee07ae02f96fb4c42ac725c48195781ee55
Alex Devero <deveroalex@gmail.com>
2018-12-06 06:31:20 +0100

    Add Security page

---
+++ src/pages/security/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../../components/layout'
+
+const DeveroSecurity = () => (
+  <Layout>
+    <h1>Hi from the Devero Security page</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default DeveroSecurity
