e0193c7cd256afaa71b3a38c8acdbd727e6881ed
Alex Devero <deveroalex@gmail.com>
2018-12-08 07:27:45 +0100

    Update particles component

--- src/components/particles.js
+++ src/components/particles.js
@@ -9,63 +9,65 @@ export class ParticlesComponent extends React.Component {
       <Particles
         width="320"
         height="320"
+        className="particles-canvas__wrapper"
+        canvasClassName="particles-canvas-deer"
         params={{
-          fps_limit: 28,
-          particles: {
-            number: {
-              value: 200,
-              density: {
-                enable: false
+          'fps_limit': 28,
+          'particles': {
+            'number': {
+              'value': 200,
+              'density': {
+                'enable': false
               }
             },
-            line_linked: {
-              enable: true,
-              distance: 30,
-              opacity: 0.4
+            'line_linked': {
+              'enable': true,
+              'distance': 30,
+              'opacity': 0.4
             },
-            move: {
-              speed: 1
+            'move': {
+              'speed': 1
             },
-            opacity: {
-              anim: {
-                enable: true,
-                opacity_min: 0.05,
-                speed: 2,
-                sync: false
+            'opacity': {
+              'anim': {
+                'enable': true,
+                'opacity_min': 0.05,
+                'speed': 2,
+                'sync': false
               },
-              value: 0.4
+              'value': 0.4
             }
           },
-          polygon: {
-            enable: true,
-            scale: 0.5,
-            type: 'inline',
-            move: {
-              radius: 10
+          'polygon': {
+            'enable': true,
+            'scale': 0.5,
+            'type': 'inline',
+            'move': {
+              'radius': 10
             },
             url: require('./../images/deer.svg'),
-            inline: {
-              arrangement: 'equidistant'
+            'inline': {
+              'arrangement': 'equidistant'
             },
-            draw: {
-              enable: true,
-              stroke: {
-                color: 'rgba(0, 0, 0, .2)'
+            'draw': {
+              'enable': true,
+              'stroke': {
+                'color': 'rgba(1, 1, 1, .2)'
               }
             }
           },
-          retina_detect: false,
-          interactivity: {
-            events: {
-              onhover: {
-                enable: true,
-                mode: 'bubble'
+          'retina_detect': false,
+          'interactivity': {
+            'events': {
+              'onhover': {
+                'enable': true,
+                'mode': 'bubble'
               }
             },
-            modes: {
-              bubble: {
-                size: 6,
-                distance: 40
+            'modes': {
+              'bubble': {
+                'size': 6,
+                'distance': 40
               }
             }
           }
