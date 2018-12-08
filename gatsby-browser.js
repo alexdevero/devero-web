27c5bf1eed6106f32e60d2126eafb6b83e40d8e8
Alex Devero <deveroalex@gmail.com>
2018-12-08 07:25:33 +0100

    Add pollyfill for svg used by react-particles-js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onClientEntry = () => {
+  // Add pollyfill for svg used by react-particles-js on page load
+  const node = document.createElement('script')
+  node.setAttribute('src', 'https://cdn.rawgit.com/progers/pathseg/master/pathseg.js')
+
+  document.head.appendChild(node)
+}
