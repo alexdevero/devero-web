98c167cb782531896930396771886d17eaca7f7d
Alex Devero <deveroalex@gmail.com>
2019-09-16 05:37:57 +0200

    Add & implement gatsby-plugin-typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,7 @@ module.exports = {
         icon: 'src/images/favicon.png' // This path is relative to the root of the site.
       }
     },
+    'gatsby-plugin-typescript',
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
