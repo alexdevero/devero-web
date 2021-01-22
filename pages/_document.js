1fbb8ba9ebb62d94a65d4e4d69c47cda1bb2d200
Alex Devero <deveroalex@gmail.com>
2021-01-22 13:54:52 +0100

    Update meta tags and stylesheets

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
 
           <meta name="keywords" content="startups, products, company, innovation, technology, web design, web development, nanotechnology, financial, space, media, industries, robotics, security, ventures, ai, genomics" />
 
-          <meta name="description" content="Devero corporation - build successful products and companies faster." />
+          <meta name="description" content="Devero corporation - build successful products and companies better and faster." />
 
           <link
             rel="apple-touch-icon"
@@ -52,13 +52,26 @@ class MyDocument extends Document {
 
           <meta name="theme-color" content="#000" />
 
-          <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700&amp;subset=latin-ext" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet" />
+
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap" rel="stylesheet" />
+
+          {/* <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700&amp;subset=latin-ext" rel="stylesheet" /> */}
 
           <link
             rel="stylesheet"
             href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
             integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
             crossOrigin="anonymous"
+          />
+
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"
+            integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA=="
+            crossorigin="anonymous"
           />
         </Head>
 
