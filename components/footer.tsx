7e985d441d0dbfecec30a3094ec3cf54ca1a805b
Alex Devero <deveroalex@gmail.com>
2021-01-23 16:03:51 +0100

    Make fotter links smaller

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,4 @@
-import React from 'react'
 import Link from 'next/link'
-
-const handleFooterLinkClick = (event: MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-  event.preventDefault()
-}
 
 const Footer = () => (
   <footer className="footer bg-black">
@@ -11,38 +6,32 @@ const Footer = () => (
       <div>
         <ul className="nav d-flex justify-content-center">
           <li className="nav-item">
-            <Link href="/">
-              <a className="nav-link">DEVERO</a>
-            </Link>
-          </li>
-
-          <li className="nav-item">
             <Link href="/about-us/">
-              <a className="nav-link">About us</a>
+              <a className="nav-link"><small>About us</small></a>
             </Link>
           </li>
 
           <li className="nav-item">
             <a className="nav-link" href="https://www.blog.alexdevero.com" rel="noopener noreferrer" target="_blank">
-              Blog
+              <small>Blog</small>
             </a>
           </li>
 
           <li className="nav-item">
             <Link href="/privacy-policy/">
-              <a className="nav-link">Privacy policy</a>
+              <a className="nav-link"><small>Privacy policy</small></a>
             </Link>
           </li>
 
           <li className="nav-item">
             <Link href="/terms-conditions/">
-              <a className="nav-link">Terms &amp; conditions</a>
+              <a className="nav-link"><small>Terms &amp; conditions</small></a>
             </Link>
           </li>
         </ul>
       </div>
 
-      <div className="mt-2">
+      <div className="mt-1">
         <ul className="nav d-flex justify-content-center">
           <li className="nav-item">
             <a href="https://twitter.com/alexdevero" className="nav-link" rel="noopener noreferrer" target="_blank">
