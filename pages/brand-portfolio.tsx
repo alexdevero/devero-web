0b8ea9fce14125deafb58144944b741a456fed94
Alex Devero <deveroalex@gmail.com>
2021-01-23 13:29:27 +0100

    Rewrite brand portfolio page

--- pages/brand-portfolio.tsx
+++ pages/brand-portfolio.tsx
@@ -1,16 +1,12 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import Link from 'next/link'
 
 import Layout from '../components/layout'
-import SEO from '../components/seo'
 
 const BrandPortfolio = () => (
-  <Layout>
-    <SEO title="Brand folio" keywords={['company', 'innovation', 'technology', 'web design', 'web development', 'nanotechnology', 'financial', 'space', 'media', 'industries', 'robotics', 'security', 'ventures', 'ai', 'genomics']} />
-
+  <Layout title="Brand folio | Devero">
     <h1>Hi from the Brand folio page</h1>
 
-    <Link to="/">Go back to the homepage</Link>
+    <Link href="/">Go back to the homepage</Link>
   </Layout>
 )
 
