d340f76b51b6d48ca1254849159e9a7b1447e964
Alex Devero <deveroalex@gmail.com>
2021-01-23 13:29:55 +0100

    Rewrite about us page

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,16 +1,12 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import Link from 'next/link'
 
 import Layout from '../components/layout'
-import SEO from '../components/seo'
 
 const AboutUs = () => (
-  <Layout>
-    <SEO title="About us" keywords={['company', 'innovation', 'technology', 'web design', 'web development', 'nanotechnology', 'financial', 'space', 'media', 'industries', 'robotics', 'security', 'ventures', 'ai', 'genomics']} />
-
+  <Layout title="About Us | Devero">
     <h1>Hi from the About us page</h1>
 
-    <Link to="/">Go back to the homepage</Link>
+    <Link href="/">Go back to the homepage</Link>
   </Layout>
 )
 
