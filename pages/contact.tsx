48e041cabe85b7dcadec56492c7f918208cdba86
Alex Devero <deveroalex@gmail.com>
2021-01-26 16:00:10 +0100

    Add contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/layout'
+
+export default function Contact() {
+  return (
+    <Layout title="Contact | Devero">
+      <h1>Hi from the Contact page</h1>
+    </Layout>
+  )
+}
