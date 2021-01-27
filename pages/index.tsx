451df1f9a0ec8c32da15df4a22226ae0fd97fb4e
Alex Devero <deveroalex@gmail.com>
2021-01-27 14:09:39 +0100

    Update homepage usp

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,8 @@ export default function Index() {
 
           <h2 className="lead">Build successful products and companies better, faster and more intelligently.</h2>
 
+          <p>9 out of 10 start-ups fail. We will help you be part of that 10% of start-ups that succeed. With our expertise and experience, you can improve the odds of your success.</p>
+
           <div className="row justify-content-center">
             <div className="col-md-8">
               <Timer />
