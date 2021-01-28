a1067a7de33dc460a102a21d0552830151f2368c
Alex Devero <deveroalex@gmail.com>
2021-01-28 10:10:19 +0100

    Add form state

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,25 @@
+import { useState } from 'react'
 import Layout from '../components/layout'
 
 export default function Contact() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const updateState = (type: String, payload: String) => {
+    switch (type) {
+      case 'name':
+        setName(payload)
+        break
+      case 'email':
+        setEmail(payload)
+        break
+      case 'message':
+        setMessage(payload)
+        break
+    }
+  }
+
   return (
     <Layout title="Contact | Devero">
       <h1>Hi from the Contact page</h1>
