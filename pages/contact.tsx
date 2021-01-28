771e257061a75690d1c1e8d3af65415aa692a015
Alex Devero <deveroalex@gmail.com>
2021-01-28 11:12:38 +0100

    Add html form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -24,7 +24,57 @@ export default function Contact() {
 
   return (
     <Layout title="Contact | Devero">
-      <h1>Hi from the Contact page</h1>
+      <h1>Contact us</h1>
+
+      <p>Do you have an idea for a new product? Do you want to start a new start-up, or even build a new unicorn? Let&apos;s get in touch!</p>
+
+      <div className="contact-form">
+        <div className="form-row">
+          <div className="col-md-6">
+            <label htmlFor="formName">Full name:</label>
+
+            <input
+              required
+              type="text"
+              className="form-control"
+              name="formName"
+              id="formName"
+              onChange={(event: HTMLInputElement) => updateState('name', event.value)}
+            />
+
+            <div className="invalid-feedback">
+              Please provide a valid name.
+            </div>
+          </div>
+
+          <div className="col-md-6">
+            <label htmlFor="formEmail">Email:</label>
+
+            <input
+              required
+              type="text"
+              className="form-control"
+              name="formEmail"
+              id="formEmail"
+              onChange={(event: HTMLInputElement) => updateState('email', event.value)}
+            />
+
+            <div className="invalid-feedback">
+              Please select a valid email.
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="formEmail">Message (optional)</label>
+
+          <textarea
+            name="formMessage"
+            id="formMessage"
+            onChange={(event: HTMLTextAreaElement) => updateState('message', event.value)}
+          />
+        </div>
+      </div>
     </Layout>
   )
 }
