7b5c12941a2d3471bcb9767608ac869c9b417901
Alex Devero <deveroalex@gmail.com>
2021-01-30 11:19:03 +0100

    Add specific heading class

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -6,6 +6,6 @@ interface PageHeaderUI {
 
 export const PageHeader = (props: PageHeaderUI) => (
   <div className="pt-4 pb-4 text-center">
-    <h1>{props.title}</h1>
+    <h1 className="h1">{props.title}</h1>
   </div>
 )
