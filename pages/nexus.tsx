e2d6cf6609c255fc4409d993257ad97ccb03c5a6
Alex Devero <deveroalex@gmail.com>
2021-01-30 12:19:48 +0100

    Create secret page

---
+++ pages/nexus.tsx
@@ -0,0 +1,10 @@
+import Layout from './../components/layout'
+import { PageHeader } from './../components/page-header'
+
+const Nexus = () => (
+  <Layout title="Master Plan | Devero">
+    <PageHeader title="Master Plan" />
+  </Layout>
+)
+
+export default Nexus
