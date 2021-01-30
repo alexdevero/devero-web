71c40e37675fe8c8147df6102c904055e3ca7e31
Alex Devero <deveroalex@gmail.com>
2021-01-30 11:18:38 +0100

    Add content for about us page

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -7,41 +7,63 @@ const AboutUs = () => (
   <Layout title="About Us | Devero" isFluid>
     <PageHeader title="About us" />
 
-    {/* <Link href="/">Go back to the homepage</Link> */}
-
-    <div className="row text-center">
-      <div className="col md-6 about-box">
-        <div className="about-box-title">Startup garage</div>
-
-        <p></p>
-
-        <Link href="/about-us">Learn more &gt;</Link>
-      </div>
-
-      <div className="col md-6 about-box">
-        <div className="about-box-title">Product lab</div>
-
-        <p></p>
-
-        <Link href="/about-us">Learn more &gt;</Link>
+    <div className="container pb-3">
+      <div className="row justify-content-center">
+        <div className="col-md-8 col-lg-6 text-center">
+          <p className="m-0">Our mission is two-fold. First, it is to help anyone with an idea to create a startup that will succeed. Second, it is to help startups, companies, governments and also individuals to build great products users will love.</p>
+        </div>
       </div>
     </div>
 
     <div className="row text-center">
       <div className="col md-6 about-box">
-        <div className="about-box-title">Startup incubator</div>
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <h3 className="h4 about-box-title m-0">Startup garage</h3>
 
-        <p>Program to help new startups succeed. Workspace, seed funding, mentoring and training to get from initial idea to successful business.</p>
+            <p className="my-3">Taking care of creating your startup, even managing it till it becomes profitable. From development, marketing, sales, to business development.</p>
 
-        <Link href="/about-us">Learn more &gt;</Link>
+            <Link href="/about-us">Learn more &gt;</Link>
+          </div>
+        </div>
       </div>
 
       <div className="col md-6 about-box">
-        <div className="about-box-title">Startup accelerator</div>
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <h3 className="h4 about-box-title m-0">Product lab</h3>
 
-        <p>Taking startups to the next level of their development. Leverage their initial successes, fine tune business models, meet potential investors.</p>
+            <p className="my-3">Turning your idea into a real product for you. We help you analyze, validate, plan, build, design, and release your product to market.</p>
 
-        <Link href="/about-us">Learn more &gt;</Link>
+            <Link href="/about-us">Learn more &gt;</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="row text-center">
+      <div className="col md-6 about-box">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <h3 className="h4 about-box-title m-0">Startup incubator</h3>
+
+            <p className="my-3">Helping new startups succeed. Workspace, seed funding, mentoring and training necessary to get from initial idea to successful business.</p>
+
+            <Link href="/about-us">Learn more &gt;</Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="col md-6 about-box">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <h3 className="h4 about-box-title m-0">Startup accelerator</h3>
+
+            <p className="my-3">Taking startups to the next level of their development. Leverage initial successes, fine tune business models, meet potential investors.</p>
+
+            <Link href="/about-us">Learn more &gt;</Link>
+          </div>
+        </div>
       </div>
     </div>
   </Layout>
