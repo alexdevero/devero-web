3350d1aa0dbee524edc70910e7de872ccabaac08
Alex Devero <deveroalex@gmail.com>
2021-02-01 10:36:03 +0100

    Create page for web design and development

---
+++ pages/web.tsx
@@ -0,0 +1,10 @@
+import Layout from './../components/layout'
+import { PageHeader } from './../components/page-header'
+
+const Web = () => (
+  <Layout title="Web design & development | Devero">
+    <PageHeader title="Web design & development" />
+  </Layout>
+)
+
+export default Web
