ddec4b0358907c3e424190468c414e35b97a681c
Alex Devero <deveroalex@gmail.com>
2021-02-15 12:42:53 +0100

    Add divider to page header

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -7,5 +7,7 @@ interface PageHeaderUI {
 export const PageHeader = (props: PageHeaderUI) => (
   <div className="pt-4 pb-4 text-center">
     <h1 className="h1">{props.title}</h1>
+
+    <hr className="divider" />
   </div>
 )
