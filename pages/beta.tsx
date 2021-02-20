6b63608f9e73e6d90329314921bfd3703bf17879
Alex Devero <deveroalex@gmail.com>
2021-02-20 13:32:30 +0100

    Start working on new homepage

---
+++ pages/beta.tsx
@@ -0,0 +1,13 @@
+import Layout from '../components/layout'
+import FooterBig from '../components/footer-big'
+
+const Beta = () => (
+  <>
+    <Layout title="Home | Devero" pageName="homepage">
+    </Layout>
+
+    <FooterBig />
+  </>
+)
+
+export default Beta
