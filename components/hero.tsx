36174a716cc59ed1cee0f6cdae67c2274fa7e019
Alex Devero <deveroalex@gmail.com>
2021-02-20 15:07:23 +0100

    Add hero component

---
+++ components/hero.tsx
@@ -0,0 +1,15 @@
+const Hero = () => (
+  <div className="hero jumbotron bg-white px-0">
+    <div className="row">
+      <div className="col-md-10 col-lg-8">
+        {/* <h1 className="display-3 font-bebas">DEVERO Corporation</h1> */}
+
+        <h2 className="display-4">We help build<br/> successful products<br/> &amp; companies</h2>
+
+        <p>90% of start-ups and products fail. We will help you be part of those 10% that succeed. With our expertise, you can improve the odds of your success.</p>
+      </div>
+    </div>
+  </div>
+)
+
+export default Hero
