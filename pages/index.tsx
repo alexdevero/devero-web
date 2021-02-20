9e046141c49c33ddfd4c13e531b740dcf790d0d5
Alex Devero <deveroalex@gmail.com>
2021-02-20 15:07:00 +0100

    Replace homepage with new version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,10 @@
+import Hero from '../components/hero'
 import Layout from '../components/layout'
-import { Timer } from '../components/timer'
 
-export default function Index() {
-  return (
-    <Layout title="Home | Devero" pageName="homepage">
-      <div className="dashboard__header">
-        <div className="dashboard__content">
-          <h1 className="display-2 font-bebas">DEVERO Corporation</h1>
+const Beta = () => (
+  <Layout title="Home | Devero" pageName="homepage">
+    <Hero />
+  </Layout>
+)
 
-          <h2 className="lead">Build successful products and companies better, faster and more intelligently.</h2>
-
-          <div className="row justify-content-center">
-            <div className="col-md-10 col-lg-8">
-              <p>9 out of 10 start-ups fail. We will help you be part of that 10% of start-ups that succeed. With our expertise and experience, you can improve the odds of your success.</p>
-            </div>
-          </div>
-
-          <div className="row justify-content-center">
-            <div className="col-md-8">
-              <Timer />
-            </div>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  )
-}
+export default Beta
