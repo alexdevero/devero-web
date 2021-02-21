675f5a1b56513b90edf5ad2ccc8ee30e7042d3d8
Alex Devero <deveroalex@gmail.com>
2021-02-21 16:15:05 +0100

    Add component with stats

---
+++ components/current-stats.tsx
@@ -0,0 +1,29 @@
+const CurrentStats = () => (
+  <div className="row py-4">
+    <div className="col-md-6 col-lg-3 text-center mt-md-down-2">
+      <p className="h5 font-weight-light m-0">+350k</p>
+
+      <p className="small font-weight-normal mt-1 mb-0">Users</p>
+    </div>
+
+    <div className="col-md-6 col-lg-3 text-center">
+      <p className="h5 font-weight-light m-0">4</p>
+
+      <p className="small font-weight-normal mt-1 mb-0">Products</p>
+    </div>
+
+    <div className="col-md-6 col-lg-3 text-center mt-md-down-2">
+      <p className="h5 font-weight-light m-0">2</p>
+
+      <p className="small font-weight-normal mt-1 mb-0">Startups</p>
+    </div>
+
+    <div className="col-md-6 col-lg-3 text-center mt-md-down-2">
+      <p className="h5 font-weight-light m-0">2</p>
+
+      <p className="small font-weight-normal mt-1 mb-0">Countries</p>
+    </div>
+  </div>
+)
+
+export default CurrentStats
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import CurrentStats from '../components/current-stats'
 import Hero from '../components/hero'
 import Layout from '../components/layout'
 
 const Beta = () => (
   <Layout title="Home | Devero" pageName="homepage">
     <Hero />
+
+    <CurrentStats />
   </Layout>
 )
 
