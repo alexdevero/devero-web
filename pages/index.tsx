2dd7bb6e78d45562deed59dbef75c3a5fd3734b3
Alex Devero <deveroalex@gmail.com>
2021-02-27 16:09:08 +0100

    Add roadmap component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import CurrentStats from '../components/current-stats'
 import Hero from '../components/hero'
 import Layout from '../components/layout'
+import RoadMap from '../components/roadmap'
 
 const Beta = () => (
   <Layout title="Home | Devero" pageName="homepage">
     <Hero />
 
     <CurrentStats />
+
+    <RoadMap />
   </Layout>
 )
 
