47750d3eb9ff1e87273429416412480af3292d62
Alex Devero <deveroalex@gmail.com>
2021-02-27 14:51:03 +0100

    Update styles of current stats

--- components/current-stats.tsx
+++ components/current-stats.tsx
@@ -1,27 +1,27 @@
 const CurrentStats = () => (
-  <div className="row py-4">
+  <div className="row py-5">
     <div className="col-6 col-md-3 text-center">
-      <p className="h5 font-weight-light m-0">+350k</p>
+      <p className="h4 font-weight-bold font-roboto-condensed m-0">+350k</p>
 
-      <p className="small font-weight-normal mt-1 mb-0">Users</p>
+      <p className="small text-uppercase font-roboto-condensed font-weight-bold mt-1 mb-0">Users</p>
     </div>
 
     <div className="col-6 col-md-3 text-center">
-      <p className="h5 font-weight-light m-0">4</p>
+      <p className="h4 font-weight-bold font-roboto-condensed m-0">4</p>
 
-      <p className="small font-weight-normal mt-1 mb-0">Products</p>
+      <p className="small text-uppercase font-roboto-condensed font-weight-bold mt-1 mb-0">Products</p>
     </div>
 
     <div className="col-6 col-md-3 text-center mt-md-down-2">
-      <p className="h5 font-weight-light m-0">2</p>
+      <p className="h4 font-weight-bold font-roboto-condensed m-0">2</p>
 
-      <p className="small font-weight-normal mt-1 mb-0">Startups</p>
+      <p className="small text-uppercase font-roboto-condensed font-weight-bold mt-1 mb-0">Startups</p>
     </div>
 
     <div className="col-6 col-md-3 text-center mt-md-down-2">
-      <p className="h5 font-weight-light m-0">2</p>
+      <p className="h4 font-weight-bold font-roboto-condensed m-0">2</p>
 
-      <p className="small font-weight-normal mt-1 mb-0">Countries</p>
+      <p className="small text-uppercase font-roboto-condensed font-weight-bold mt-1 mb-0">Countries</p>
     </div>
   </div>
 )
