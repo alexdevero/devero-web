8e9fac1d100fe69301bc5a011d8ba7aefc6708a8
Alex Devero <deveroalex@gmail.com>
2021-02-27 16:08:50 +0100

    Add more weights for roboto typeface

--- pages/_document.js
+++ pages/_document.js
@@ -56,7 +56,7 @@ class MyDocument extends Document {
 
           <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet" />
 
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
 
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet" />
 
