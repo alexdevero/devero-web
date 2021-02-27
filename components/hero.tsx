be02e896ce9e1360c88ac4e79d419bb64389a9a5
Alex Devero <deveroalex@gmail.com>
2021-02-27 16:07:59 +0100

    Udpate hero typography styles

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => (
 
         <h2 className="display-4">We help build<br/> successful products<br/> &amp; companies</h2>
 
-        <p>90% of start-ups and products fail. We will help you be part of those 10% that succeed. With our expertise, you can improve the odds of your success.</p>
+        <p className="h5 font-weight-normal">90% of start-ups and products fail. We will help you be part of those 10% that succeed. With our expertise, you can improve the odds of your success.</p>
       </div>
     </div>
   </div>
