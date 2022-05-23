375796517c0ce81e04af3a3d2ba44d44d0fca7ad
Alex Devero <deveroalex@gmail.com>
2022-05-23 12:44:06 +0200

    Update home page text

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,12 +3,12 @@ import { memo } from 'react'
 export const Hero = memo(() => (
   <div className="hero jumbotron bg-white px-0">
     <div className="row py-5">
-      <div className="col-md-10 col-lg-8">
+      <div className="col-md-8 col-lg-6">
         {/* <h1 className="display-3 font-bebas">DEVERO Corporation</h1> */}
 
-        <h2 className="display-4">We help build<br/> successful products<br/> &amp; companies</h2>
+        <h2 className="display-4 mb-2">We build products people love</h2>
 
-        <p className="h5 font-weight-normal">90% of start-ups and products fail. We will help you be part of those 10% that succeed. With our expertise, you can improve the odds of your success.</p>
+        <p className="h5 font-weight-normal">90% of start-ups fail. One thing that can cause failure is product. We will help you create and deliver product your users will love. With our expertise, you can improve the odds of success and be part of those 10% that succeed.</p>
       </div>
     </div>
   </div>
