ea8f78d58585dbceba4e905d783c572b07c2426b
Alex Devero <deveroalex@gmail.com>
2022-05-23 17:15:43 +0200

    Add firebase app check

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp, getApps } from 'firebase/app'
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check'
 import { getAnalytics } from 'firebase/analytics'
 
 export const createFirebaseApp = () => {
@@ -15,6 +16,12 @@ export const createFirebaseApp = () => {
 
   if (getApps().length <= 0) {
     const app = initializeApp(clientCredentials)
+    const appCheck = initializeAppCheck(app, {
+      provider: new ReCaptchaV3Provider('6LfodhEgAAAAAJicTrp0S03dy5vtbjrHnQOeJguU'),
+      // Optional argument. If true, the SDK automatically refreshes App Check
+      // tokens as needed.
+      isTokenAutoRefreshEnabled: true
+    })
 
     // Check that `window` is in scope for the analytics module!
     if (typeof window !== 'undefined') {
