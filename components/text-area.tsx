b800a7b0473602e2a517a7ed966716d9e99a664e
Alex Devero <deveroalex@gmail.com>
2022-05-23 13:10:03 +0200

    Add component for text area

---
+++ components/text-area.tsx
@@ -0,0 +1,23 @@
+import { memo } from 'react'
+
+interface TextAreaProps {
+  id: string;
+  fieldName: string;
+  label: string;
+  required?: boolean;
+  onChange: (fieldName: string, value: string) => void;
+}
+
+export const TextArea = memo((props: TextAreaProps) => (
+  <>
+    <label htmlFor="formEmail">{props.label}</label>
+
+    <textarea
+      className="form-control"
+      id={props.id}
+      name={props.id}
+      required={props.required}
+      onChange={(event: ChangeEvent<HTMLTextAreaElement>) => props.onChange(props.fieldName, event.target.value)}
+    />
+  </>
+))
