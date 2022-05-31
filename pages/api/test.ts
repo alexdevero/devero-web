db03d8c55204eec10170a502828b07fdb7a164ca
Alex Devero <deveroalex@gmail.com>
2022-05-31 10:53:22 +0200

    Add demo api route

---
+++ pages/api/test.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.json({
+    text: 'Test route',
+    code: 200,
+    time: new Date()
+  })
+}
