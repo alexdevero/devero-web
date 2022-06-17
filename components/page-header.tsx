c64233386219d05fc25dea1cdf2a3550ac0e2af4
Alex Devero <deveroalex@gmail.com>
2022-06-17 12:55:32 +0200

    Change page header heading style

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -6,7 +6,7 @@ interface PageHeaderUI {
 
 export const PageHeader = memo((props: PageHeaderUI) => (
   <div className="pt-4 pb-4 text-center">
-    <h1 className="h1">{props.title}</h1>
+    <h1 className="h4">{props.title}</h1>
 
     <hr className="divider" />
   </div>
