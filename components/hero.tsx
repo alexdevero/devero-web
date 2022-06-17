4bb25c1d61e4ef054012625a545191ee4a76e4bc
Alex Devero <deveroalex@gmail.com>
2022-06-17 12:40:14 +0200

    Update hero text, extract box to a component

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,19 +6,17 @@ export const Hero = memo(() => {
 
   return (
     <div className="hero jumbotron px-0">
-      <div className="box" />
-
       <div className="row py-5">
         <div className="col-md-8 col-lg-7">
           {/* <h1 className="display-3 font-bebas">DEVERO Corporation</h1> */}
 
           <h1 className="h4 display mb-2">
-            {intl.formatMessage({ defaultMessage: 'Next-level digital{br} products and solutions' }, {
+            {intl.formatMessage({ defaultMessage: 'We build digital{br} products and solutions' }, {
               br: <br />
             })}
           </h1>
 
-          <p className="body1 lead">{intl.formatMessage({ defaultMessage: 'We are a full-stack software studio that builds digital products and solutions for customer-centric companies, products optimized for user experience and performance.' })}</p>
+          <p className="body1 lead">{intl.formatMessage({ defaultMessage: 'We are a full-stack software studio that builds digital products and solutions for companies of any size, products optimized for user experience and performance.' })}</p>
         </div>
       </div>
     </div>
