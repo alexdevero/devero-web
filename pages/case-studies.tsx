533ec58ba4d901887def739a9fcc08103dcde522
Alex Devero <deveroalex@gmail.com>
2022-06-17 12:55:17 +0200

    Update case studies page layout

--- pages/case-studies.tsx
+++ pages/case-studies.tsx
@@ -1,14 +1,30 @@
 import Link from 'next/link'
 import { memo } from 'react'
+import { useIntl } from 'react-intl'
 
 import { Layout } from '../components/layout'
+import { PageHeader } from '../components/page-header'
 
-const CaseStudies = memo(() => (
-  <Layout title="Case studies | Devero">
-    <h1>Hi from the Case studies page</h1>
+import { routes } from '../data/routes'
 
-    <Link href="/">Go back to the homepage</Link>
-  </Layout>
-))
+const CaseStudies = memo(() => {
+  const intl = useIntl()
+
+  return (
+    <Layout title="Case studies | Devero">
+      <PageHeader title={intl.formatMessage({ defaultMessage: 'Case studies' })} />
+
+      <div className="container pb-3">
+        <div className="row justify-content-center">
+          <div className="col-md-8 col-lg-6 text-center">
+            <p className="mt-0 mb-2">{intl.formatMessage({ defaultMessage: 'This page is under construction...' })}</p>
+
+            <Link href={routes.root}><a>&larr; {intl.formatMessage({ defaultMessage: 'Go back home' })}</a></Link>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+})
 
 export default CaseStudies
